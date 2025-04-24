cc42394af4f7ec5d7a603dfb4c8403e80888e081
NoranAhmeddd <noranahmed941@gmail.com>
2025-04-24 13:59:00 +0200

    new message

--- Main.java
+++ Main.java
@@ -4,6 +4,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -14,6 +17,7 @@ public class Main extends Application {
         stage.setScene(new Scene(root));
         stage.show();
     }
+    
 
     public static void main(String[] args) {
         launch(args);
--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Compiling...
-javac -d bin --module-path "C:\javafx-sdk-21.0.7\lib" --add-modules javafx.controls,javafx.fxml Main.java
+javac -d bin --module-path "C:\Users\a_sal\OneDrive\Desktop\Modern Gas\javafx-sdk-24.0.1\lib" --add-modules javafx.controls,javafx.fxml Main.java
 echo Copying resources...
 xcopy /Y /I *.fxml bin\
 xcopy /Y /I *.css bin\
@@ -9,5 +9,5 @@ xcopy /Y /I *.jpg bin\
 xcopy /Y /I *.gif bin\
 xcopy /Y /I *.xml bin\
 echo Running...
-java -cp bin --module-path "C:\javafx-sdk-21.0.7\lib" --add-modules javafx.controls,javafx.fxml Main
+java -cp bin --module-path "C:\Users\a_sal\OneDrive\Desktop\Modern Gas\javafx-sdk-24.0.1\lib" --add-modules javafx.controls,javafx.fxml Main
 pause 
