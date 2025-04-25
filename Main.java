a573ff79c7016d7e9e36a90cf004e2f479eea441
Mohamed Sabry <mohamed.2520076@stemelsadat.moe.edu.eg>
2025-04-25 16:32:43 +0300

    more clean structure for the project Files

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("mainThemeDesign2.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("mainThemeDesign.fxml"));
         stage.setTitle("JavaFX App");
         stage.setScene(new Scene(root));
         stage.show();
